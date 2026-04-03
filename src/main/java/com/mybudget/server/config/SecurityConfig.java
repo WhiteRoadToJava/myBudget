d5a5cdca313092d66cb76dc99bf0062782b24e66
Mohammad Abbas <msaabbas6@gmail.com>
2026-04-04 00:31:19 +0200

    create a key

--- src/main/java/com/mybudget/server/config/SecurityConfig.java
+++ src/main/java/com/mybudget/server/config/SecurityConfig.java
@@ -68,7 +68,7 @@ private String frontendUrl;
         CorsConfiguration configuration = new CorsConfiguration();
 
         // only allow request from our future react client
-        configuration.setAllowedOrigins(List.of(
+        configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
                 "https://my-budget-frontend.vercel.app",
                 "https://dreamy-cajeta-ce3d15.netlify.app",
