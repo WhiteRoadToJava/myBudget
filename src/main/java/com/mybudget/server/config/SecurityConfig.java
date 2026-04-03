5a205d4389aca7a0dcf635748126ac4003e80344
Mohammad Abbas <msaabbas6@gmail.com>
2026-04-04 00:20:45 +0200

    create a key

--- src/main/java/com/mybudget/server/config/SecurityConfig.java
+++ src/main/java/com/mybudget/server/config/SecurityConfig.java
@@ -72,6 +72,7 @@ private String frontendUrl;
                 "http://localhost:5173",
                 "https://my-budget-frontend.vercel.app",
                 "https://dreamy-cajeta-ce3d15.netlify.app",
+                "https://my-budget-frontend-*.vercel.app",
                 "https://my-budget-frontend-production.up.railway.app",
                 frontendUrl
         ));
