f5a1e9e0bd8c2bdbadb26bd016038ae7e218d831
WhiteRoatOfJava <msaabbas6@gmail.com>
2026-06-23 03:55:41 +0200

    fix the register function

--- src/main/java/com/mybudget/server/controllers/UserControll.java
+++ src/main/java/com/mybudget/server/controllers/UserControll.java
@@ -1,9 +1,16 @@
 package com.mybudget.server.controllers;
 
+import com.mybudget.server.dto.RegisterRequest;
+import com.mybudget.server.dto.RegisterResponse;
 import com.mybudget.server.dto.user.UserRequset;
 import com.mybudget.server.dto.user.UserResponse;
 import com.mybudget.server.services.UserService;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,4 +36,38 @@ private final UserService userService;
 }
 
 
+
+@PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        // check if username already exists
+        if(userService.existsByUsername(registerRequest.getUsername())) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body("Username already exists.");
+        }
+
+        // map the AuthRequest to a User entity
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(registerRequest.getPassword());
+
+        // assign roles
+        if(registerRequest.getRoles() == null || registerRequest.getRoles().isEmpty()) {
+            user.setRoles(Set.of(Role.USER));
+        } else {
+            user.setRoles(registerRequest.getRoles());
+        }
+
+        // register the user using UserService
+        userService.registerUser(user);
+
+        // create respons object
+        RegisterResponse response = new RegisterResponse(
+                "User registered successfully",
+                user.getUsername(),
+                user.getRoles()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
